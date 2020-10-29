9ae3156ea29580969d39c4b253213467e723895b
AdrianCo97 <adrian.corazza@outlook.com>
2020-10-29 14:18:41 +0100

    Ändrade artikelnummer i bok filen.

--- src/Main.java
+++ src/Main.java
@@ -356,6 +356,8 @@ public class Main {
 		for (int i = 0; i < mediaList.size(); i++) {
 			if (mediaList.get(i).articleNumber == articleNumber) {
 				mediaList.remove(i);
+				
+				System.out.println("Successfully deregistered " + mediaList.get(i).title);
 			}
 
 		}
